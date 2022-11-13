b6c3366d777f1a0099efe6b645ae400f4861e306
Chris Carrick <36552610+cjcarrick@users.noreply.github.com>
2022-11-12 19:02:26 -0800

    prepare for ashley's files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import MapContainer from '../components/MapContainer.vue'
-
 </script>
 <template>
   <div id="app">
@@ -11,29 +10,30 @@ import MapContainer from '../components/MapContainer.vue'
 </template>
 
 <style>
-  html, body {
-    height: 100%;
-    margin: 0;
-  }
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
 
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    display: grid;
-    grid-template-columns: 100vw;
-    grid-auto-rows: 7.5em;
-    grid-gap: 0rem;
-    padding: 1rem;
-    box-sizing: border-box;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: grid;
+  grid-template-columns: 100vw;
+  grid-auto-rows: 7.5em;
+  grid-gap: 0rem;
+  padding: 1rem;
+  box-sizing: border-box;
+}
 
-  .cell {
-    border-radius: 4px;
-    background-color: rgb(141, 75, 75);
-  }
+.cell {
+  border-radius: 4px;
+  background-color: rgb(141, 75, 75);
+}
 
-  .cell-map {
-    grid-column: 1;
-    grid-row-start: 1;
-    grid-row-end: 3;
-  }
+.cell-map {
+  grid-column: 1;
+  grid-row-start: 1;
+  grid-row-end: 3;
+}
 </style>
